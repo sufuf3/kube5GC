b4b968bd6ae6857f6c16bc971d718043544654fc
dobie <dobiechiu@nctu.edu.tw>
2018-12-04 21:42:11 -0800

    create Josn SBI file to SMF

---
+++ src/smf/smf_json_handler.c
@@ -0,0 +1,11 @@
+#include <cJSON/cJSON.h>
+
+#include "core_debug.h"
+#include "core_pkbuf.h"
+#include "3gpp_types.h"
+#include "smf_json_handler.h"
+
+status_t smf_json_handler_create_session(pkbuf_t **pkbuf)
+{
+    return CORE_OK;
+}
