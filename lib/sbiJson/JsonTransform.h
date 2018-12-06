a562e49e967ccadcae5b50133b2cc5d624bf6daa
Weiting Hu <zxc6752@gmail.com>
2018-12-06 19:27:32 +0800

    code refactor

--- lib/sbiJson/JsonTransform.h
+++ lib/sbiJson/JsonTransform.h
@@ -30,8 +30,24 @@ typedef struct _creat_session_t {
     c_uint8_t       rat_type[10];
 } creat_session_t;
 
+typedef struct _modify_bearer_t {
+    c_int8_t        imsi_bcd[MAX_IMSI_BCD_LEN+1];
+    c_uint8_t       uli_presence;
+    tai_t           tai;
+    e_cgi_t         e_cgi;
+    plmn_id_t       visited_plmn_id;
+    guti_t          guti; 
+    c_uint8_t       rat_type[10];
+    c_uint8_t       ebi;
+    ip_t            enb_s1u_ip;
+    c_uint32_t      enb_s1u_teid;
+} modify_bearer_t;
+
 CORE_DECLARE(status_t) JSONTRANSFORM_StToJs_create_session_request(creat_session_t *sess, cJSON *pJson);
 CORE_DECLARE(status_t) JSONTRANSFORM_JsToSt_create_session_request(creat_session_t *sess, cJSON *pJson);
+CORE_DECLARE(status_t) JSONTRANSFORM_StToJs_modify_bearer_request(modify_bearer_t *sess, cJSON *pJson);
+CORE_DECLARE(status_t) JSONTRANSFORM_JsToSt_modify_bearer_request(modify_bearer_t *sess, cJSON *pJson);
+
 #ifdef __cplusplus
 }
 #endif 
