c582209c33a90d5eaa7c7b386380060f3b3f3f4d
Fu-Lian Weng <turtle11311@gmail.com>
2018-12-19 12:29:53 +0800

    Remove S11 Handler and Build

--- src/smf/smf_n11_build.h
+++ src/smf/smf_n11_build.h
@@ -10,13 +10,6 @@
 extern "C" {
 #endif /* __cplusplus */
 
-CORE_DECLARE(status_t) smf_s11_build_create_session_response(
-        pkbuf_t **pkbuf, smf_sess_t *sess);
-CORE_DECLARE(status_t) smf_s11_build_delete_session_response(
-        pkbuf_t **pkbuf, smf_sess_t *sess);
-CORE_DECLARE(status_t) smf_s11_build_modify_bearer_response(
-        pkbuf_t **pkbuf, smf_sess_t *sess);
-
 CORE_DECLARE(status_t) smf_n11_build_create_session_response(
         pkbuf_t **pkbuf, smf_sess_t *sess);
         
--- src/smf/smf_n11_handler.h
+++ src/smf/smf_n11_handler.h
@@ -10,13 +10,6 @@
 extern "C" {
 #endif /* __cplusplus */
 
-CORE_DECLARE(void) smf_s11_handle_create_session_request(gtp_xact_t *s11_xact,
-        smf_sess_t *sess, gtp_create_session_request_t *req);
-CORE_DECLARE(void) smf_s11_handle_delete_session_request(gtp_xact_t *s11_xact,
-        smf_sess_t *sess, gtp_delete_session_request_t *req);
-CORE_DECLARE(void) smf_s11_handle_modify_bearer_request(gtp_xact_t *s11_xact,
-        smf_sess_t *sess, gtp_modify_bearer_request_t *req);
-
 CORE_DECLARE(void) smf_n11_handle_create_session_request_by_JsonCreateSession(smf_sess_t *sess, create_session_t *createSession);
 CORE_DECLARE(void) smf_n11_handle_update_session_request_by_JsonUpdateSession(smf_sess_t *sess, modify_bearer_t *pModifyBearer);
 CORE_DECLARE(void) smf_n11_handle_delete_session_request_by_JsonDeleteSession(smf_sess_t *sess, delete_session_t *pDeleteSession);
