adc81a1408e97712b8d8675c631c4cbf4ccd2a84
dobie <dobiechiu@nctu.edu.tw>
2018-12-25 10:51:24 +0800

    add debug log to amf json handler.

--- src/amf/amf_json_handler.c
+++ src/amf/amf_json_handler.c
@@ -78,8 +78,7 @@ status_t amf_json_handle_create_session(pkbuf_t **pkbuf, amf4g_sess_t *pSess) {
     ori_bearer = amf4g_default_bearer_in_sess(pSess);
     d_assert(ori_bearer, return CORE_ERROR, "Null param");
     cJSON *session = cJSON_Parse((*pkbuf)->payload);
-    // string = cJSON_Print(session);
-    // d_info(string);
+    
     d_assert(session, return CORE_ERROR, "Null param");
 
     amf4g_ue = core_calloc(1, sizeof(amf4g_ue_t));
@@ -92,81 +91,98 @@ status_t amf_json_handle_create_session(pkbuf_t **pkbuf, amf4g_sess_t *pSess) {
     memcpy(amf4g_ue->imsi, createSession.imsi, createSession.imsi_len);
     amf4g_ue->imsi_len = createSession.imsi_len;
 
-    if(strcmp(amf4g_ue->imsi_bcd, ori_amf4g_ue->imsi_bcd) != 0) {
+    if(strcmp(amf4g_ue->imsi_bcd, ori_amf4g_ue->imsi_bcd) != 0) 
+    {
         d_error("imsi_data Error");
     }
     
-    if(amf4g_ue->imsi_len != ori_amf4g_ue->imsi_len) {
+    if(amf4g_ue->imsi_len != ori_amf4g_ue->imsi_len) 
+    {
         d_error("imsi_len Error %d %d", amf4g_ue->imsi_len, ori_amf4g_ue->imsi_len);    
     }
+
     /* tai */
     memcpy(&amf4g_ue->tai.tac, &createSession.tai.tac, sizeof(createSession.tai.tac));
     memcpy(&amf4g_ue->tai.plmn_id, &createSession.tai.plmn_id, sizeof(createSession.tai.plmn_id));
     /* e-cgi */
     memcpy(&amf4g_ue->e_cgi.cell_id, &createSession.e_cgi.cell_id, sizeof(createSession.e_cgi.cell_id));
     memcpy(&amf4g_ue->e_cgi.plmn_id, &createSession.e_cgi.plmn_id, sizeof(createSession.e_cgi.plmn_id));
-    if(!compare_plmnid(amf4g_ue->tai.plmn_id, ori_amf4g_ue->tai.plmn_id)){
+    if(!compare_plmnid(amf4g_ue->tai.plmn_id, ori_amf4g_ue->tai.plmn_id))
+    {
         d_error("tai.plmn_id Error");
     }        
-    if(amf4g_ue->tai.tac != ori_amf4g_ue->tai.tac) {
+    if(amf4g_ue->tai.tac != ori_amf4g_ue->tai.tac) 
+    {
         d_error("tai.tac Error");
     }
-    if(!compare_plmnid(amf4g_ue->e_cgi.plmn_id, ori_amf4g_ue->e_cgi.plmn_id)) {
+    if(!compare_plmnid(amf4g_ue->e_cgi.plmn_id, ori_amf4g_ue->e_cgi.plmn_id)) 
+    {
         d_error("e_cgi.plmn_id Error");
     }     
-    if(amf4g_ue->e_cgi.cell_id != ori_amf4g_ue->e_cgi.cell_id) {
+    if(amf4g_ue->e_cgi.cell_id != ori_amf4g_ue->e_cgi.cell_id) 
+    {
         d_error("e_cgi.cell_id Error");
     }
     /*visited plmn id */
     memcpy(&amf4g_ue->visited_plmn_id, &createSession.visited_plmn_id, sizeof(createSession.visited_plmn_id));    
-    if(!compare_plmnid(amf4g_ue->visited_plmn_id, ori_amf4g_ue->visited_plmn_id)){
+    if(!compare_plmnid(amf4g_ue->visited_plmn_id, ori_amf4g_ue->visited_plmn_id))
+    {
         d_error("visited_plmn_id Error");
     }
 
     char rat_type[10]; 
     memcpy(rat_type, createSession.rat_type , sizeof(createSession.rat_type));   
-    if(strcmp(rat_type, "EUTRA") != 0) {
+    if(strcmp(rat_type, "EUTRA") != 0) 
+    {
         d_error("rat_type Error");
     }
         
     /* packet data network */
     pdn->pdn_type = createSession.pdn.pdn_type;
     pdn->paa.pdn_type = createSession.pdn.paa.pdn_type;
-    // d_info("pdn->paa.pdn_type:%d pdn->pdn_type:%d", pdn->paa.pdn_type, pdn->pdn_type);
+
     if(pdn->paa.pdn_type != ori_pdn->paa.pdn_type)
+    {
         d_error("paa.pdn_type Error pdn->paa.pdn_type:%d ori_pdn->paa.pdn_type:%d", pdn->paa.pdn_type, ori_pdn->paa.pdn_type);
-    
-    if (pdn->paa.pdn_type == SBI_PDN_TYPE_IPV4){
+    }
+
+    if (pdn->paa.pdn_type == SBI_PDN_TYPE_IPV4)
+    {
         pdn->paa.addr = createSession.pdn.paa.addr;
     }
-    else if (pdn->paa.pdn_type == SBI_PDN_TYPE_IPV6){
+    else if (pdn->paa.pdn_type == SBI_PDN_TYPE_IPV6)
+    {
         pdn->paa.len = createSession.pdn.paa.len;
         memcpy(pdn->paa.addr6, createSession.pdn.paa.addr6, sizeof(createSession.pdn.paa.addr6));
     }
-    else if (pdn->paa.pdn_type == SBI_PDN_TYPE_IPV4V6){
+    else if (pdn->paa.pdn_type == SBI_PDN_TYPE_IPV4V6)
+    {
         pdn->paa.both.addr = createSession.pdn.paa.addr;
         pdn->paa.both.len = createSession.pdn.paa.len;
         memcpy(pdn->paa.addr6, createSession.pdn.paa.addr6, sizeof(createSession.pdn.paa.addr6));
     }
-    else {
-        // d_assert(0, return CORE_ERROR, "Not supported(%d)", pdn->pdn_type);
+    else 
+    {
          d_error( "Not supported(%d)", pdn->pdn_type);
     }
         
     pdn->ambr.uplink = createSession.pdn.ambr.uplink;
     pdn->ambr.downlink = createSession.pdn.ambr.downlink;
     
-    if(pdn->ambr.uplink != ori_pdn->ambr.uplink) {
+    if(pdn->ambr.uplink != ori_pdn->ambr.uplink) 
+    {
         d_error("ambr.uplink Error %d  ori_pdn->ambr.uplink :%d", pdn->ambr.uplink, ori_pdn->ambr.uplink);
     }
         
-    if(pdn->ambr.downlink != ori_pdn->ambr.downlink) {
+    if(pdn->ambr.downlink != ori_pdn->ambr.downlink) 
+    {
        d_error("ambr.downlink Error %d, ori_pdn->ambr.downlink :%d", pdn->ambr.downlink, ori_pdn->ambr.downlink);
     }  
 
     /* create bearer contexts */
     bearer->ebi = createSession.ebi;
-    if(bearer->ebi != ori_bearer->ebi) {
+    if(bearer->ebi != ori_bearer->ebi) 
+    {
         d_error("ebi Error bearer->ebi:%d, ori_bearer->ebi:%d", bearer->ebi, ori_bearer->ebi);
     }
     
@@ -186,6 +202,9 @@ status_t amf_json_handle_create_session(pkbuf_t **pkbuf, amf4g_sess_t *pSess) {
     CORE_FREE(pdn);
     
     cJSON_Delete(session);
+
+    d_trace(10, "%s %d Leave", __FUNCTION__, __LINE__);
+
     return 0;
 
 }
@@ -193,29 +212,33 @@ status_t amf_json_handle_create_session(pkbuf_t **pkbuf, amf4g_sess_t *pSess) {
 status_t amf_json_handler_create_session_response(pkbuf_t **pkbuf, create_session_t *pCreateRspSess)
 {
     cJSON *session = cJSON_Parse((*pkbuf)->payload);
-    // string = cJSON_Print(session);
-    // d_info(string);
+    
+    d_trace(10, "%s %d Enter", __FUNCTION__, __LINE__);
+    
     d_assert(session, return CORE_ERROR, "Null param");
 
     JSONTRANSFORM_JsToSt_create_session_response(pCreateRspSess, session);
     
     cJSON_Delete(session);
-    return CORE_OK;
-
+    
+    d_trace(10, "%s %d Leave", __FUNCTION__, __LINE__);
+    
     return CORE_OK;
 }
 
 status_t amf_json_handler_update_session_response(pkbuf_t **pkbuf, modify_bearer_t *modifyBearer)
 {
     cJSON *session = cJSON_Parse((*pkbuf)->payload);
-    // string = cJSON_Print(session);
-    // d_info(string);
+    
+    d_trace(10, "%s %d Enter", __FUNCTION__, __LINE__);
+
     d_assert(session, return CORE_ERROR, "Null param");
 
     JSONTRANSFORM_JsToSt_update_session_response(modifyBearer, session);
     
     cJSON_Delete(session);
     
+    d_trace(10, "%s %d Leave", __FUNCTION__, __LINE__);
 
     return CORE_OK;
 }
@@ -224,10 +247,13 @@ status_t amf_json_handler_delete_session_response(pkbuf_t **pkbuf, delete_sessio
 {
     cJSON *session = cJSON_Parse((*pkbuf)->payload);
     d_assert(session, return CORE_ERROR, "Null param");
+    
+    d_trace(10, "%s %d Enter", __FUNCTION__, __LINE__);
 
     JSONTRANSFORM_JsToSt_delete_session_response(deleteSession, session);
     
     cJSON_Delete(session);
-
+     
+    d_trace(10, "%s %d Leave", __FUNCTION__, __LINE__);
     return CORE_OK;
 }
