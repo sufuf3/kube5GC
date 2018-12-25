fe3774ce0017008a2cf18ac0e6bf1eb9e688549f
dobie <dobiechiu@nctu.edu.tw>
2018-12-25 10:41:20 +0800

    modify amf_json_build log level

--- src/amf/amf_json_build.c
+++ src/amf/amf_json_build.c
@@ -28,18 +28,11 @@ status_t amf_json_build_create_session(pkbuf_t **pkbuf, amf4g_sess_t *sess) {
     amf4g_ue = sess->amf4g_ue;
     d_assert(amf4g_ue, return CORE_ERROR, "Null param");
 
-    // d_info("createSession:%d", &createSession);
+    d_trace(10, "%s %d Enter", __FUNCTION__, __LINE__);
+
     memcpy(createSession.imsi,  amf4g_ue->imsi, amf4g_ue->imsi_len);
     createSession.imsi_len = amf4g_ue->imsi_len;
-#if 0
-    int i = 0;
-    for (i = 0; i<16; i++)
-    {
-        printf("%x ", createSession.imsi_bcd[i]);
-    }
-#endif
     memcpy(&createSession.tai, &amf4g_ue->tai, sizeof( amf4g_ue->tai));
-    // printf("\ttai :\n\t\ttac : %d\n\t\tplmn_id :\n\t\t\t\n", createSession.tai.tac);  
     memcpy(&createSession.e_cgi, &amf4g_ue->e_cgi, sizeof( amf4g_ue->e_cgi));
     memcpy(&createSession.visited_plmn_id, &amf4g_ue->visited_plmn_id, sizeof( amf4g_ue->visited_plmn_id));
     if (sess->ue_pco.length && sess->ue_pco.buffer) {
@@ -50,12 +43,12 @@ status_t amf_json_build_create_session(pkbuf_t **pkbuf, amf4g_sess_t *sess) {
     memcpy(createSession.apn, pdn->apn, sizeof(pdn->apn));
 
     c_uint8_t pdn_type;
-    d_trace(-1, "Origin PDN Type: %d\n", pdn->pdn_type);
+    d_trace(15, "%s %d Origin PDN Type: %d\n", __FUNCTION__, __LINE__, pdn->pdn_type);
     if (pdn->pdn_type == HSS_PDN_TYPE_IPV4 ||
         pdn->pdn_type == HSS_PDN_TYPE_IPV6 ||
         pdn->pdn_type == HSS_PDN_TYPE_IPV4V6)
     {
-        d_trace(-1, "PDNNNNNNNNNNNNNN %d, %d\n", pdn->pdn_type + 1, sess->request_type.pdn_type);
+        d_trace(15, "%s %d PDNNNNNNNNNNNNNN %d, %d\n", __FUNCTION__, __LINE__, pdn->pdn_type + 1, sess->request_type.pdn_type);
         pdn_type = ((pdn->pdn_type + 1) & sess->request_type.pdn_type);
         d_assert(pdn_type != 0, return CORE_ERROR,
                 "PDN Configuration Error:(%d, %d)",
@@ -71,7 +64,7 @@ status_t amf_json_build_create_session(pkbuf_t **pkbuf, amf4g_sess_t *sess) {
     memcpy(&createSession.pdn, pdn, sizeof(pdn_t));
     
     createSession.pdn.paa.pdn_type = pdn_type;
-    d_trace(-1, "createSession.pdn.paa.pdn_type :%d\n", createSession.pdn.paa.pdn_type);
+    d_trace(15, "%s %d  createSession.pdn.paa.pdn_type :%d\n", __FUNCTION__, __LINE__, createSession.pdn.paa.pdn_type);
     createSession.ebi = bearer->ebi;
     /*TODO : ue timezone*/
     memcpy(&createSession.guti, &amf4g_ue->guti, sizeof(amf4g_ue->guti));
@@ -79,7 +72,6 @@ status_t amf_json_build_create_session(pkbuf_t **pkbuf, amf4g_sess_t *sess) {
     JSONTRANSFORM_StToJs_create_session_request(&createSession, session);
 
     string = cJSON_Print(session);
-    // d_info(string);
 
     length = strlen(string) + 1;
     
@@ -88,6 +80,8 @@ status_t amf_json_build_create_session(pkbuf_t **pkbuf, amf4g_sess_t *sess) {
     memcpy((*pkbuf)->payload, string, length -1);
     free(string);
     cJSON_Delete(session);
+
+    d_trace(10, "%s %d Leave", __FUNCTION__, __LINE__);
 
     return CORE_OK;
 }
@@ -101,10 +95,10 @@ CORE_DECLARE(status_t) amf_json_build_modify_bearer(pkbuf_t **pkbuf, amf4g_beare
 
     d_assert(bearer, return CORE_ERROR, "Null param");
     
-    // c_uint32_t      enb_s1u_teid;
+    d_trace(10, "%s %d Enter", __FUNCTION__, __LINE__);
+    
     memcpy(modifyBearer.imsi, bearer->amf4g_ue->imsi, bearer->amf4g_ue->imsi_len);
     modifyBearer.imsi_len = bearer->amf4g_ue->imsi_len; 
-    //TODO: fix it how to get uli presence?
     memcpy(&modifyBearer.uli_presence, "\x01" , sizeof(c_uint8_t));
     memcpy(&modifyBearer.tai, &bearer->amf4g_ue->tai, sizeof(bearer->amf4g_ue->tai));
     memcpy(&modifyBearer.e_cgi, &bearer->amf4g_ue->e_cgi, sizeof(bearer->amf4g_ue->e_cgi));
@@ -117,7 +111,7 @@ CORE_DECLARE(status_t) amf_json_build_modify_bearer(pkbuf_t **pkbuf, amf4g_beare
     memcpy(&modifyBearer.enb_s1u_teid, &bearer->enb_s1u_teid, sizeof(bearer->enb_s1u_teid));
     memcpy(&modifyBearer.apn, &bearer->sess->pdn->apn, sizeof( bearer->sess->pdn->apn));
     modifyBearer.sm_context_update_type = sm_context_update_type;
-    d_trace(5, "JSONTRANSFORM_StToJs_modify_bearer_request");
+    
     JSONTRANSFORM_StToJs_modify_bearer_request(&modifyBearer, j_modifyBearerReq);
     string = cJSON_Print(j_modifyBearerReq);
     d_info(string);
@@ -127,6 +121,8 @@ CORE_DECLARE(status_t) amf_json_build_modify_bearer(pkbuf_t **pkbuf, amf4g_beare
     memcpy((*pkbuf)->payload, string, length -1);
     free(string);
     cJSON_Delete(j_modifyBearerReq);
+
+    d_trace(10, "%s %d Leave", __FUNCTION__, __LINE__);
     return 0;
 }
 
@@ -136,6 +132,8 @@ status_t amf_json_build_delete_session(pkbuf_t **pkbuf, amf4g_sess_t *sess)
     cJSON *j_deleteSession = cJSON_CreateObject();
     delete_session_t deleteSession = {0};
     c_uint32_t length = 0;
+    
+    d_trace(10, "%s %d Enter", __FUNCTION__, __LINE__);
     
     /* imsi */
     memcpy(deleteSession.imsi, sess->amf4g_ue->imsi, sess->amf4g_ue->imsi_len);
@@ -147,16 +145,21 @@ status_t amf_json_build_delete_session(pkbuf_t **pkbuf, amf4g_sess_t *sess)
     /* ebi */
     deleteSession.ebi = amf4g_default_bearer_in_sess(sess)->ebi;
 
-    d_trace(5, "JSONTRANSFORM_StToJs_modify_bearer_request");
     JSONTRANSFORM_StToJs_delete_session_request(&deleteSession, j_deleteSession);
     string = cJSON_Print(j_deleteSession);
-    d_trace(-1, "Delete Session JSON: %s", string);
+    d_trace(15, "Delete Session JSON: %s", string);
     
     length = strlen(string) + 1;
     *pkbuf = pkbuf_alloc(0, length);
+    d_assert(*pkbuf != NULL, return CORE_ERROR, "*pkbuf is null");
+    
     (*pkbuf)->len = length;
     memcpy((*pkbuf)->payload, string, length -1);
+        
     free(string);
     cJSON_Delete(j_deleteSession);
+
+    d_trace(10, "%s %d Leave", __FUNCTION__, __LINE__);
+
     return CORE_OK;
 }
