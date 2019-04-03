0cf4941995742b463cc49ec588d4ad7ac6d4b183
Samina Fu <sufuf3@gmail.com>
2019-04-03 11:59:08 +0800

    Update docker file

--- docker/nextepc-build/setup.sh
+++ docker/nextepc-build/setup.sh
@@ -5,6 +5,9 @@ if ! grep "uptun" /proc/net/dev > /dev/null; then
 fi
 ip addr del 45.45.0.1/16 dev uptun 2> /dev/null
 ip addr add 45.45.0.1/16 dev uptun
-ip addr del cafe::1/64 dev uptun 2> /dev/null
-ip addr add cafe::1/64 dev uptun
+#ip addr del cafe::1/64 dev uptun 2> /dev/null
+#ip addr add cafe::1/64 dev uptun
 ip link set uptun up
+sh -c 'echo 1 > /proc/sys/net/ipv4/ip_forward'
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -I INPUT -i uptun -j ACCEPT
